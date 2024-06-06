2fc292a2bfc37ef255cd3893c33710aa210c0b39
lucasfersant <lucas10fs3@gmail.com>
2024-06-06 15:17:30 -0300

    Add files via upload

---
+++ aula15.py
@@ -0,0 +1,67 @@
+# # 1: Verificando se o número é par ou ímpar
+# numero = int(input('>>'))%2==0
+
+# match numero:
+#              case  True :
+#                     print('par')
+#              case _:
+#                     print('impar')
+
+
+
+
+
+
+# # 2: Verificando se um número é positivo, negativo ou zero
+# n = int(input('nmr >>'))
+# match n:
+#      case n if n == 0:
+#          print('zero')
+#      case n if n < 0:
+#          print('negativo')
+#      case n if n > 0:
+#          print("positivo")
+
+
+
+
+
+# # 3: Verificando se uma string é vazia ou não
+# palavra = input('>>>')
+# match palavra:
+#              case 'text':
+#                  print ('cheia')
+#              case _:
+#                   print('vazia')
+
+
+
+
+
+
+# # 4: Verificando se um número é maior, menor ou igual a 10
+# n = int(input('nar >>'))
+# match n:
+#         case n if n == 10:
+#          print('é 10')
+
+#         case n if n > 10:
+#         case _:
+#             print('menor que 10')
+
+
+
+
+
+# # 5: Classificando uma idade em faixas etárias
+# idade = int(input('idade'))
+# match idade:
+#             case idade if idade >= 65:
+#                  print('idoso')
+#             case idade if idade > 18:
+#                  print('adolescente')
+#             case idade if idade >= 18 and idade <= 65:
+#                  print('adulto')
+
+
+
